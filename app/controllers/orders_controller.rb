3e952a2a2fcc751dcf224dbef8514b6b3aedfd65
Tino Thamjarat <tino@oozou.com>
2016-09-03 10:46:19 +0700

    temporary remove stock deducting from order controller and enable negative discount

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,9 +25,11 @@ class OrdersController < ApplicationController
    if order.save
      current_user.cart.cart_products.each do |item|
        OrderProduct.create(product_id: item.product.id, order_id: order.id, amount: item.amount)
-       updated_stock = item.product.stock - item.amount
-       item.product.stock = updated_stock
-       item.product.save
+       
+      # FIXME remove comment after adding all previous order
+      #  updated_stock = item.product.stock - item.amount
+      #  item.product.stock = updated_stock
+      #  item.product.save
      end
    end
    current_user.cart.clear
